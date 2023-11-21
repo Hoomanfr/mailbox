8f0a7fa9ffd43d826b08fcb237a2be0e687a0e80
Hooman Familrouhani <hooman.fr@gmail.com>
2023-11-20 20:56:11 -0800

    pub/sub sample

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
 	"os"
 
+	"github.com/thumperq/golib/logging"
 	httpserver "github.com/thumperq/golib/servers/http"
-	"github.com/thumperq/wms/mailbox/api"
 )
 
 func main() {
-	env := &api.Env{}
+	env := &Env{}
 	exitCode := <-httpserver.ListenAndServe(env.Bootstrap)
+	err := env.Broker.Disconnect()
+	if err != nil {
+		logging.TraceLogger(context.Background()).
+			Err(err).
+			Msg("error disconnecting from broker")
+	}
 	os.Exit(exitCode)
 }
