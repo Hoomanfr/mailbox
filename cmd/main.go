9ab7e64d6f903c0f8bacd79697b79678d327a5ff
hoomanfr <hooman.fr@gmail.com>
2024-09-06 09:51:18 -0700

    factories and env

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"context"
-	"os"
 
 	"github.com/thumperq/golib/application"
 	"github.com/thumperq/golib/database"
 	"github.com/thumperq/golib/environment"
 	"github.com/thumperq/golib/logging"
 	"github.com/thumperq/golib/messaging"
-	httpserver "github.com/thumperq/golib/servers/http"
 	"github.com/thumperq/wms/mailbox/api"
 	"github.com/thumperq/wms/mailbox/internal/app"
 	"github.com/thumperq/wms/mailbox/internal/consumers"
@@ -18,46 +16,52 @@ import (
 )
 
 func main() {
-	env, err := environment.NewEnv()
-	if err != nil {
-		logging.TraceLogger(context.Background()).
-			Err(err).
-			Msg("error creating new environment")
-		os.Exit(1)
-	}
-	err = env.Bootstrap(bootstrap)
+	err := environment.NewEnv().
+		WithBroker().
+		WithDbFactory().
+		WithAppFactory().
+		Bootstrap(bootstrap)
+
 	if err != nil {
 		logging.TraceLogger(context.Background()).
 			Err(err).
 			Msg("error bootstrapping environment")
 	}
 }
-func bootstrap(env *environment.Env, apiSrv *httpserver.ApiServer) error {
+
+func bootstrap(env *environment.Env) error {
+	setupDbs(env)
+	setupApps(env)
+	setupApis(env)
+	runSubscribers(env)
+	return nil
+}
+
+func setupDbs(env *environment.Env) {
 	env.DbFactory.Register(func(dbf database.DbFactory) any {
 		return db.NewUserDb(dbf.PgDb())
 	}).Register(func(dbf database.DbFactory) any {
 		return db.NewMailboxDb(dbf.PgDb())
 	})
+}
 
+func setupApps(env *environment.Env) {
 	env.AppFactory.Register(func(appFactory application.AppFactory) any {
 		return app.NewMailboxApp(env.Broker, environment.GetRepo[db.MailboxDB]())
 	}).Register(func(appFactory application.AppFactory) any {
 		return app.NewUserApp(env.Broker, environment.GetRepo[db.UserDB]())
 	})
-
-	err := runSubscribers(context.Background(), env)
-	if err != nil {
-		return err
-	}
-	api.SetupUserApi(environment.GetApp[app.UserApp](), apiSrv.Engine)
-	api.SetupMailboxApi(environment.GetApp[app.MailboxApp](), apiSrv.Engine)
-	return nil
 }
 
-func runSubscribers(ctx context.Context, env *environment.Env) error {
+func setupApis(env *environment.Env) {
+	api.SetupUserApi(environment.GetApp[app.UserApp](), env.ApiServer.Engine)
+	api.SetupMailboxApi(environment.GetApp[app.MailboxApp](), env.ApiServer.Engine)
+}
+
+func runSubscribers(env *environment.Env) error {
 	mailboxConsumers := consumers.NewMailboxConsumer(environment.GetRepo[db.MailboxDB]())
 	err := messaging.NewSubscriber(env.Broker).
-		Subscribe(ctx, "wms", "mailbox", domain.MailboxTopic, mailboxConsumers.ConsumeMailboxCreatedEvent)
+		Subscribe(context.Background(), "wms", "mailbox", domain.MailboxTopic, mailboxConsumers.ConsumeMailboxCreatedEvent)
 	if err != nil {
 		return err
 	}
